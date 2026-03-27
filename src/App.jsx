b0cab69261650ce0dbf5aec85022a80b0f5bad44
Vipul-lang <vipulrmadavi@gmail.com>
2026-03-27 10:48:37 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,38 @@
 
+import { useEffect, useState } from "react";
 import "./App.css";
 import MovieCard from "./components/MovieCard";
 import MovieProvider from "./Context/MovieProvider";
 import Favorite from "./pages/Favorite";
 
 function App() {
-  const Movies = [
+
+   const [searchQuery, setSearchQuery] = useState("");
+  const [movies,setMovies] = useState([
     { id: 1, name: "Inception" },
     { id: 2, name: "Interstellar" },
-  ];
+  ]);
+
+  useEffect(() => {
+  const filtered = movies.filter((movie) =>
+    movie.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+  setMovies(filtered);
+}, [searchQuery]);
 
   return (
     <MovieProvider>
       <h3>Movies</h3>
-      {Movies.map((movie) => (
+
+      <input
+          type="text"
+          placeholder="Search for movies..."
+          className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+
+      {movies.map((movie) => (
         <MovieCard key={movie.id} movie={movie} />
       ))}
       <Favorite/>
